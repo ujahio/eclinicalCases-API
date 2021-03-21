c7d06bba9f92c19b409823105b06c59e8ffdfad3
Joshua Abu <joshujah007@gmail.com>
2021-03-21 22:17:00 +0100

    updated test and package

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
-const sum = require('./sum');
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+        expect(true).toEqual(true);
+    });
 });
