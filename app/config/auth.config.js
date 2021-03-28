f36781532b941c6d8421a567bf941aed39a48368
Olakanmi Oluwole <wole@lagosnomad.com>
2021-03-28 09:43:45 +0100

    password reset

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -2,4 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   secret: process.env.APP_SECRET_KEY,
+  MAILER_EMAIL: process.env.MAILER_EMAIL,
+  MAILER_PASS: process.env.MAILER_PASS,
 };
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,23 +1,26 @@
-const { verifySignUp } = require("../middlewares");
-const controller = require("../controllers/auth.controller");
+const {verifySignUp} = require('../middlewares');
+const controller = require('../controllers/auth.controller');
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+        'Access-Control-Allow-Headers',
+        'x-access-token, Origin, Content-Type, Accept',
     );
     next();
   });
 
   app.post(
-    "/api/auth/signup",
-    [
-      verifySignUp.checkDuplicateUsernameOrEmail,
-      verifySignUp.checkRolesExisted
-    ],
-    controller.signup
+      '/api/auth/signup',
+      [
+        verifySignUp.checkDuplicateUsernameOrEmail,
+        verifySignUp.checkRolesExisted,
+      ],
+      controller.signup,
   );
 
-  app.post("/api/auth/signin", controller.signin);
+  app.post('/api/auth/signin', controller.signin);
+  app.post('/api/auth/forgotpassword', controller.forgotpassword);
+  app.get('/api/auth/validate/:token', controller.validatepasswordtoken);
+  app.post('/api/auth/reset/:token', controller.resetpassword);
 };
