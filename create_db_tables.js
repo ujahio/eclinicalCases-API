6ade9a05a690775270d2290bc3c9563ce5d8c35a
Tayyab Shabbir <tayyabshabbir036@gmail.com>
2024-07-08 12:42:11 +0500

    fixed case table and added delete tables script

--- create_db_tables.js
+++ create_db_tables.js
@@ -20,36 +20,31 @@ const createTables = async () => {
     },
   };
 
-const casesParams = {
-  TableName: CASES_TABLE_NAME,
-  AttributeDefinitions: [
-    { AttributeName: HASH_KEY, AttributeType: HASH_KEY_TYPE },
-    { AttributeName: "createdOn", AttributeType: "S" },
-    { AttributeName: "caseStatus", AttributeType: "S" },
-  ],
-  KeySchema: [
-    { AttributeName: HASH_KEY, KeyType: "HASH" },
-    { AttributeName: "createdOn", KeyType: "RANGE" },
-  ],
-  GlobalSecondaryIndexes: [
-    {
-      IndexName: "caseStatus-createdOn-index",
-      KeySchema: [
-        { AttributeName: "caseStatus", KeyType: "HASH" },
-        { AttributeName: "createdOn", KeyType: "RANGE" },
-      ],
-      Projection: { ProjectionType: "ALL" },
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 5,
-        WriteCapacityUnits: 5,
-      },
+  const casesParams = {
+    TableName: CASES_TABLE_NAME,
+    KeySchema: [{ AttributeName: "id", KeyType: "HASH" }],
+    AttributeDefinitions: [
+      { AttributeName: "id", AttributeType: "S" },
+      { AttributeName: "createdAt", AttributeType: "N" },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5,
     },
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 5,
-    WriteCapacityUnits: 5,
-  },
-};
+    GlobalSecondaryIndexes: [
+      {
+        IndexName: "CreatedAtIndex",
+        KeySchema: [{ AttributeName: "createdAt", KeyType: "HASH" }],
+        Projection: {
+          ProjectionType: "ALL",
+        },
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 5,
+          WriteCapacityUnits: 5,
+        },
+      },
+    ],
+  };
 
   try {
     const existingTables = await ddb.listTables();
@@ -58,14 +53,18 @@ const casesParams = {
       await ddb.createTable(usersParams);
       console.log(`Table '${USERS_TABLE_NAME}' created.`);
     } else {
-      console.log(`Table '${USERS_TABLE_NAME}' already exists. Skipping creation.`);
+      console.log(
+        `Table '${USERS_TABLE_NAME}' already exists. Skipping creation.`
+      );
     }
 
     if (!existingTables.TableNames.includes(CASES_TABLE_NAME)) {
       await ddb.createTable(casesParams);
       console.log(`Table '${CASES_TABLE_NAME}' created.`);
     } else {
-      console.log(`Table '${CASES_TABLE_NAME}' already exists. Skipping creation.`);
+      console.log(
+        `Table '${CASES_TABLE_NAME}' already exists. Skipping creation.`
+      );
     }
   } catch (error) {
     console.error("Error creating tables:", error);
