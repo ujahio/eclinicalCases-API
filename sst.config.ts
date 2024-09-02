9aa788d0c0c6c73cdbd6a13379d253908f541be6
Tayyab Shabbir <tayyabshabbir036@gmail.com>
2024-09-02 19:36:23 +0500

    SST API changes

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
+// import { Api, NextjsSite, StackContext } from "@serverless-stack/resources";
 
 export default $config({
   app(input) {
@@ -10,5 +11,29 @@ export default $config({
   },
   async run() {
     new sst.aws.Nextjs("MyWeb");
+
+    // API
+    const api = new sst.aws.ApiGatewayV2("MyApi");
+    api.route("POST /api/auth/signin", {
+      handler: "handler.handler",
+    });
+    api.route("POST /api/auth/signup", {
+      handler: "handler.handler",
+    });
+    api.route("POST /api/auth/send-otp", {
+      handler: "handler.handler",
+    });
+    api.route("POST /api/auth/reset-password", {
+      handler: "handler.handler",
+    });
+    api.route("POST /api/auth/update-password", {
+      handler: "handler.handler",
+    });
+    api.route("GET /api/auth/users", {
+      handler: "handler.handler",
+    });
+    api.route("GET /api/case/all", {
+      handler: "handler.handler",
+    });
   },
 });
