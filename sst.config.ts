c79ebbeae270ce8b9c423d8be83012bf59c8a445
Joshua Abu <otktechnologies@gmail.com>
2024-09-19 18:05:54 +0400

    linting file

--- sst.config.ts
+++ sst.config.ts
@@ -13,10 +13,13 @@ export default $config({
 		const domainName =
 			STAGE === "production" ? "eccs-online.com" : `${STAGE}.eccs-online.com`;
 
-
-		const { NEXT_JWT_SECRET, NEXT_PASS_SECRET, NEXT_PUBLIC_BASE_URL, NEXT_NODE_ENV, NEXT_PASS_SECRET_KEY } = await import(
-			"./infra/secrets"
-		);
+		const {
+			NEXT_JWT_SECRET,
+			NEXT_PASS_SECRET,
+			NEXT_PUBLIC_BASE_URL,
+			NEXT_NODE_ENV,
+			NEXT_PASS_SECRET_KEY,
+		} = await import("./infra/secrets");
 
 		const secrets = [
 			NEXT_JWT_SECRET,
@@ -27,8 +30,8 @@ export default $config({
 		];
 
 		// NextApp
-    new sst.aws.Nextjs("MyWeb", {
-      link: secrets,
+		new sst.aws.Nextjs("MyWeb", {
+			link: secrets,
 			domain: {
 				name: domainName,
 			},
