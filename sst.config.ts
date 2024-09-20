5923745f8a1bcfc75659e2a52d88eae21fc2a750
Joshua Abu <otktechnologies@gmail.com>
2024-09-20 13:12:23 +0400

    refactored sst config to use seperate client code

--- sst.config.ts
+++ sst.config.ts
@@ -9,33 +9,8 @@ export default $config({
 		};
 	},
 	async run() {
-		const STAGE = $app.stage;
-		const domainName =
-			STAGE === "production" ? "eccs-online.com" : `${STAGE}.eccs-online.com`;
-
-		const {
-			NEXT_JWT_SECRET,
-			NEXT_PASS_SECRET,
-			NEXT_PUBLIC_BASE_URL,
-			NEXT_NODE_ENV,
-			NEXT_PASS_SECRET_KEY,
-		} = await import("./infra/secrets");
-
-		const secrets = [
-			NEXT_JWT_SECRET,
-			NEXT_PASS_SECRET,
-			NEXT_PUBLIC_BASE_URL,
-			NEXT_NODE_ENV,
-			NEXT_PASS_SECRET_KEY,
-		];
-
-		// NextApp
-		new sst.aws.Nextjs("MyWeb", {
-			link: secrets,
-			domain: {
-				name: domainName,
-			},
-		});
+		// Bucket
+		await import("./infra/client");
 
 		// Bucket
 		await import("./infra/storage");
