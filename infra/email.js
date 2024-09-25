81c4b6be4412e950ef13a6ce139bfbb0fdc3e348
Joshua Abu <otktechnologies@gmail.com>
2024-09-25 22:49:43 +0400

    update gitignore; update sender's mail address; changed send params to ses v1 syntax

--- infra/email.js
+++ infra/email.js
@@ -1,3 +1,5 @@
+const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+
 export const email = new sst.aws.Email("MyEmail", {
-  sender: "eccslabs@gmail.com",
+	sender: `no-reply@${DOMAIN}.com`,
 });
--- server/services/emailSender.js
+++ server/services/emailSender.js
@@ -1,26 +1,32 @@
 import { Resource } from "sst";
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
-const sesClient = new SESClient();
+const sesClient = new SESClient({ region: "us-east-1" });
 
 export const sendEmail = async (toAddress, subject, body) => {
 	try {
 		await sesClient.send(
 			new SendEmailCommand({
-				FromEmailAddress: Resource.MyEmail.sender,
+				Source: Resource.MyEmail.sender,
 				Destination: {
 					ToAddresses: [toAddress],
 				},
-				Content: {
-					Simple: {
-						Subject: {
-							Data: subject,
+				Message: {
+					/* required */
+					Body: {
+						/* required */
+						Html: {
+							Charset: "UTF-8",
+							Data: body,
 						},
-						Body: {
-							Text: {
-								Data: body,
-							},
+						Text: {
+							Charset: "UTF-8",
+							Data: "TEXT_FORMAT_BODY",
 						},
+					},
+					Subject: {
+						Charset: "UTF-8",
+						Data: subject,
 					},
 				},
 			})
