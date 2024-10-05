a12dc128f7521b6ae02c1cc6b84f8ed904338e5f
Joshua Abu <otktechnologies@gmail.com>
2024-10-05 18:50:51 +0400

    update index of TeacherCaseStudies to include published date for accurate sorting

--- infra/dynamo.js
+++ infra/dynamo.js
@@ -15,11 +15,16 @@ export const TeacherCaseStudies = new sst.aws.Dynamo("TeacherCaseStudies", {
 		teacherId: "string", // Teacher's ID
 		caseStatus: "string", // Case study status: 'draft', 'published', or 'archived'
 		caseDeadline: "string", // Case study deadline
+		publishedDate: "string", // Date when the case study was published
 	},
 	primaryIndex: { hashKey: "id" }, // Primary key based on case_id
 	globalIndexes: {
 		TeacherStatusIndex: { hashKey: "teacherId", rangeKey: "caseStatus" }, // Index to query by teacher and case status (draft, published, archived)
 		CaseDeadlineIndex: { hashKey: "caseStatus", rangeKey: "caseDeadline" }, // Index to find cases that are about to expire
+		TeacherPublishedDateIndex: {
+			hashKey: "teacherId",
+			rangeKey: "publishedDate",
+		},
 	},
 });
 
