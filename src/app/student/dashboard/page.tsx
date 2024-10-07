f74ac4c046f4ba652b429161db55f8e6a912e0fd
Joshua Abu <otktechnologies@gmail.com>
2024-10-07 19:30:48 +0400

    correct import

--- src/app/student/dashboard/page.tsx
+++ src/app/student/dashboard/page.tsx
@@ -3,9 +3,9 @@
 import React from "react";
 import StudentDashboardComp from "@/presentation/student/Dashboard";
 import useGetArchiveCases from "@/services/hooks/useGetArchiveCases";
-import useOngoingCases from "@/services/hooks/useOngoingCases";
+import useGetActiveCase from "@/services/hooks/useGetActiveCase";
 const StudentDashboard = () => {
-	useOngoingCases();
+	useGetActiveCase();
 	useGetArchiveCases("recent"); // need to find out if we should be retrieving the same type of archived cases for students
 	return <StudentDashboardComp />;
 };
