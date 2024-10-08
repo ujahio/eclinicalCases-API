9ba8e44b41a84d852575484694f163dcf28d1539
Joshua Abu <otktechnologies@gmail.com>
2024-10-08 16:45:08 +0400

    small file name rename and restructure of published case lambda

--- server/controllers/handlePublishedCase.js
+++ server/controllers/handlePublishedCase.js
@@ -7,6 +7,7 @@ import dbClient from "../services/dbClient.js";
 import SECRETS from "../services/secrets.js";
 import { extrapolateFormData } from "../utils/api_utils.js";
 import { verifyToken } from "./case.controller.js"; // todo: move utils function to util fild/folder
+import { getCountOfStudentsFeedbacksAndResponses } from "../utils/api_utils.js";
 
 export const publishCase = async (event) => {
 	const {
@@ -153,6 +154,104 @@ export const publishCase = async (event) => {
 			body: JSON.stringify({
 				message: "Error publishing case.",
 				error: error.message,
+			}),
+		};
+	}
+};
+
+export const getPublishedCase = async (event) => {
+	const userToken = event.headers.authorization.split(" ")[1];
+	const userInfo = verifyToken(userToken, SECRETS.NEXT_JWT_SECRET);
+
+	if (
+		!userInfo &&
+		!(userInfo.user_role === "teacher" || userInfo.user_role === "student")
+	) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "Not authorized to view this resource",
+			}),
+		};
+	}
+
+	if (!userInfo.id) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "Invalid input: missing required fields",
+			}),
+		};
+	}
+
+	try {
+		const params = {
+			TableName: TABLES.TEACHER_CASE_STUDIES,
+			IndexName: "TeacherStatusIndex",
+			KeyConditionExpression:
+				"teacherId = :teacherId AND caseStatus = :caseStatus",
+			ExpressionAttributeValues: {
+				":teacherId": userInfo.id,
+				":caseStatus": "published",
+			},
+		};
+
+		if (userInfo.user_role === "student") {
+			params.ExpressionAttributeValues[":teacherId"] = userInfo.teacherId;
+		}
+
+		const command = new QueryCommand(params);
+		const result = await dbClient.send(command);
+		const activeCaseResult = result.Items[0];
+
+		// If no active cases found, return a message
+		if (!activeCaseResult) {
+			return {
+				statusCode: 200,
+				body: JSON.stringify({
+					message: "There are no ongoing cases at the moment.",
+				}),
+			};
+		}
+
+		const caseInfo = {
+			id: activeCaseResult.id,
+			caseTopic: activeCaseResult.caseTopic,
+			createdAt: activeCaseResult.createdAt,
+			caseDeadline: activeCaseResult.caseDeadline,
+			caseStatus: activeCaseResult.caseStatus,
+		};
+
+		if (userInfo.user_role === "teacher") {
+			const countOfStudentsFeedbackAndResponses =
+				await getCountOfStudentsFeedbacksAndResponses(activeCaseResult.id);
+
+			return {
+				statusCode: 200,
+				body: JSON.stringify({
+					message: "Ongoing case retrieved successfully!",
+					caseInfo: {
+						...caseInfo,
+						feedbackCount: countOfStudentsFeedbackAndResponses.feedbackCount,
+						totalResponses: countOfStudentsFeedbackAndResponses.totalResponses,
+					},
+				}),
+			};
+		} else {
+			return {
+				statusCode: 200,
+				body: JSON.stringify({
+					message: "Ongoing case retrieved successfully!",
+					caseInfo,
+				}),
+			};
+		}
+	} catch (error) {
+		console.error("Error retrieving ongoing case:", error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				error: `Could not retrieve ongoing cases: ${error.message}`,
 			}),
 		};
 	}
