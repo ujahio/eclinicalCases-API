d860a846397873b58334533f863b87b0ebc905c0
Joshua Abu <otktechnologies@gmail.com>
2024-10-24 15:01:19 +0400

    refactored "useRenderPdf" into its own hook

--- src/presentation/student/Certificates.tsx
+++ src/presentation/student/Certificates.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import * as pdfjsLib from "pdfjs-dist";
-import "pdfjs-dist/build/pdf.worker.mjs";
+import useRenderPdf from "@/services/hooks/useRenderPdf";
 import Modal from "@/components/ui/Modal";
 import DashboardLayout from "@/components/layouts/dashboard";
 import Button from "../../components/ui/Button";
@@ -18,63 +17,18 @@ const Certificates = ({
 	const [showCertModal, setShowCertModal] = useState(false);
 	const [selectedPdf, setSelectedPdf] = useState<string | null>(null);
 	const modalCanvasRef = useRef<HTMLCanvasElement | null>(null);
+	const { renderPdf } = useRenderPdf();
 
 	const certPopUp = (pdfBase64: string) => {
 		setSelectedPdf(pdfBase64);
 		setShowCertModal(true);
 	};
 
-	const renderPdf = async (
-		pdfBase64: string,
-		canvasRef: React.RefObject<HTMLCanvasElement>,
-		scale = 1.0
-	) => {
-		if (!pdfBase64 || !canvasRef.current) return;
-
-		try {
-			const base64String = pdfBase64.split(",")[1];
-
-			if (!base64String) {
-				throw new Error("Base64 content not found in the provided data URL.");
-			}
-
-			const pdfData = atob(base64String);
-			const pdfArray = new Uint8Array(pdfData.length);
-
-			for (let i = 0; i < pdfData.length; i++) {
-				pdfArray[i] = pdfData.charCodeAt(i);
-			}
-
-			const pdf = await pdfjsLib.getDocument({ data: pdfArray }).promise;
-			const page = await pdf.getPage(1);
-
-			const viewport = page.getViewport({ scale });
-
-			const canvas = canvasRef.current;
-			const context = canvas.getContext("2d");
-
-			if (!context) {
-				throw new Error("Canvas context is not available.");
-			}
-			canvas.height = viewport.height;
-			canvas.width = viewport.width;
-
-			const renderContext = {
-				canvasContext: context,
-				viewport: viewport,
-			};
-
-			await page.render(renderContext).promise;
-		} catch (error) {
-			console.error("Error rendering PDF:", error);
-		}
-	};
-
 	useEffect(() => {
 		if (selectedPdf && modalCanvasRef.current) {
 			renderPdf(selectedPdf, modalCanvasRef);
 		}
-	}, [selectedPdf]);
+	}, [selectedPdf, renderPdf]);
 
 	return (
 		<DashboardLayout>
