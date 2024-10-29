506a6da4640378c0a200317501d830829312523c
Joshua Abu <otktechnologies@gmail.com>
2024-10-29 20:58:21 +0400

    add infrastructure code for cognito userpool to create users

--- infra/client.js
+++ infra/client.js
@@ -3,11 +3,16 @@ import {
 	NEXT_PUBLIC_PASS_SECRET_KEY,
 	NEXT_PUBLIC_BASE_URL,
 	NEXT_PUBLIC_NODE_ENV,
+	NEXT_AUTH_SECRET,
+	NEXT_PUBLIC_DOMAIN,
 } from "./secrets";
+import { authUrl, userPool, eccsWebClient } from "./auth";
 
 const STAGE = $app.stage;
 const domainName =
-	STAGE === "production" ? "eccs-online.com" : `${STAGE}.eccs-online.com`;
+	STAGE === "production"
+		? NEXT_PUBLIC_DOMAIN
+		: `${STAGE}.${NEXT_PUBLIC_DOMAIN}`;
 
 export const client = new sst.aws.Nextjs("MyWeb", {
 	link: [
@@ -15,8 +20,16 @@ export const client = new sst.aws.Nextjs("MyWeb", {
 		NEXT_PUBLIC_BASE_URL,
 		NEXT_PUBLIC_NODE_ENV,
 		NEXT_PUBLIC_PASS_SECRET_KEY,
+		// authUrl,
+		userPool,
+		eccsWebClient,
 	],
 	domain: {
 		name: domainName,
+	},
+	environment: {
+		AUTH_SECRET: process.env.NEXT_AUTH_SECRET,
+		NEXT_PUBLIC_REGION: aws.getRegionOutput().name,
+		NEXT_PUBLIC_USER_POOL_DOMAIN: authUrl,
 	},
 });
