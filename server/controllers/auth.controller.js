61360331dcd7742a7c29b95df02f6562fa0be590
Joshua Abu <otktechnologies@gmail.com>
2024-10-31 21:44:50 +0400

    update lambda to use cognito for registration of uses (teacher for now)

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,14 @@
 import jwt from "jsonwebtoken";
+import { Resource } from "sst";
 import dbClient from "../services/dbClient.js";
 import bcrypt from "bcryptjs";
-import { v4 as uuidv4 } from "uuid";
-import { PutCommand, ScanCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import {
+	AdminCreateUserCommand,
+	AdminSetUserPasswordCommand,
+	AdminGetUserCommand,
+	ListUsersCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 import {
 	updateUserPassword,
 	generateOtp,
@@ -14,7 +20,7 @@ import {
 import { TABLES } from "../services/dbTables.js";
 import { resources } from "../services/resources.js";
 import { sendEmail } from "../services/emailSender.js";
-import { checkDuplicateUsernameOrEmail } from "../middlewares/verifySignUp";
+import cognitoClient from "../services/cognitoClient.js";
 
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -22,6 +28,7 @@ export const signup = async (event) => {
 	const { firstName, lastName, email, password, user_role } = JSON.parse(
 		event.body
 	);
+
 	try {
 		// Validate input fields
 		if (!firstName || typeof firstName !== "string") {
@@ -49,31 +56,46 @@ export const signup = async (event) => {
 			};
 		}
 
-		const duplicateCheckResponse = await checkDuplicateUsernameOrEmail(event);
-
-		if (duplicateCheckResponse) {
-			return duplicateCheckResponse;
+		// Check if the email is already registered in Cognito
+		try {
+			const checkUserCommand = new AdminGetUserCommand({
+				UserPoolId: Resource.eccs.id,
+				Username: email,
+			});
+			await cognitoClient.send(checkUserCommand);
+			return {
+				statusCode: 400,
+				body: JSON.stringify({
+					message: "A user with this email already exists.",
+				}),
+			};
+		} catch (error) {
+			if (error.name !== "UserNotFoundException") {
+				console.error("Error checking for existing user:", error);
+				return {
+					statusCode: 500,
+					body: JSON.stringify({
+						message: "Error checking for existing user.",
+						error: `Error checking for existing user: ${error.message}`,
+					}),
+				};
+			}
 		}
 
 		let teacherId;
-
-		// Fetch the single teacher's ID from DynamoDB
+		// Fetch the first available teacher's ID from Cognito if the user role is 'student'
 		if (user_role === "student") {
-			const teacherParams = {
-				TableName: TABLES.USER,
-				IndexName: "RoleIndex", // Use user_role instead of role
-				KeyConditionExpression: "user_role = :user_role", // Use user_role
-				ExpressionAttributeValues: {
-					":user_role": "teacher",
-				},
-			};
+			const listTeachersCommand = new ListUsersCommand({
+				UserPoolId: Resource.eccs.id,
+				Filter: 'custom:user_role = "teacher"',
+				Limit: 1,
+			});
 
-			const teacherCommand = new QueryCommand(teacherParams);
-			const teacherResult = await dbClient.send(teacherCommand);
+			const teachers = await cognitoClient.send(listTeachersCommand);
 
-			if (!teacherResult.Items || teacherResult.Items.length === 0) {
+			if (!teachers.Users || teachers.Users.length === 0) {
 				return {
-					statusCode: 500, // TODO: check for correct status, it should be 400, not 500
+					statusCode: 400,
 					body: JSON.stringify({
 						error: "No teacher found in the system.",
 					}),
@@ -81,61 +103,217 @@ export const signup = async (event) => {
 			}
 
 			// Get the first (and only) teacher's ID
-			teacherId = teacherResult.Items[0].id;
+			teacherId = teachers.Users[0].Username;
 		}
 
-		// Prepare the new user data
+		// Register the new user in Cognito
+		const userAttributes = [
+			{ Name: "email", Value: email },
+			{ Name: "custom:firstName", Value: firstName },
+			{ Name: "custom:lastName", Value: lastName },
+			{ Name: "custom:user_role", Value: user_role },
+		];
+		// Only add the teacherId attribute for students
+		if (user_role === "student" && teacherId) {
+			userAttributes.push({ Name: "custom:teacherId", Value: teacherId });
+		}
+
 		const originalPassword = decryptPassword(
 			password,
 			resources.NEXT_PUBLIC_PASS_SECRET_KEY
 		);
-		const hashedPassword = bcrypt.hashSync(originalPassword, 10);
-		const userId = uuidv4();
-		const createdAt = new Date().toISOString();
 
-		const user = {
-			id: userId,
-			firstName,
-			lastName,
-			email,
-			password: hashedPassword,
-			createdAt,
-			status: "active",
-			signUpLevel: 1,
-			paymentStatus: "inactive",
-			user_role,
-			teacherId: user_role === "student" ? teacherId : null,
+		const signupParams = {
+			UserPoolId: Resource.eccs.id,
+			Username: email,
+			TemporaryPassword: originalPassword, // Cognito requires an initial temporary password
+			UserAttributes: userAttributes,
+			MessageAction: "SUPPRESS", // Prevent sending automatic confirmation email
 		};
 
-		const params = {
-			TableName: TABLES.USER,
-			Item: user,
-		};
+		const createUserCommand = new AdminCreateUserCommand(signupParams);
+		const signupResponse = await cognitoClient.send(createUserCommand);
 
-		const command = new PutCommand(params);
-		await dbClient.send(command);
+		// Set the password to permanent by authenticating the user with the new password
+		const setPasswordCommand = new AdminSetUserPasswordCommand({
+			UserPoolId: Resource.eccs.id,
+			Username: email,
+			Password: originalPassword,
+			Permanent: true,
+		});
+		await cognitoClient.send(setPasswordCommand);
 
-		// Remove password before sending response
-		delete user.password;
+		console.log("signupResponse", signupResponse);
 
+		// Return a success response without password information
 		return {
 			statusCode: 201,
 			body: JSON.stringify({
-				message: "User was registered successfully!",
-				data: user,
+				message: "User was registered successfully! Please verify your email.",
+				userSub: signupResponse.User?.Username,
 			}),
 		};
 	} catch (error) {
-		console.error("Error creating new user:", error);
+		console.error("Error registering user:", error);
 		return {
 			statusCode: 500,
 			body: JSON.stringify({
-				details: `Error creating new user: ${error.message}`,
 				message: "Error creating new user.",
+				error: error.message,
 			}),
 		};
 	}
 };
+
+// export const signup = async (event) => {
+// 	const { firstName, lastName, email, password, user_role } = JSON.parse(
+// 		event.body
+// 	);
+// 	try {
+// 		// Validate input fields
+// 		if (!firstName || typeof firstName !== "string") {
+// 			return {
+// 				statusCode: 400,
+// 				body: JSON.stringify({ message: '"firstName" must be a string' }),
+// 			};
+// 		}
+// 		if (!lastName || typeof lastName !== "string") {
+// 			return {
+// 				statusCode: 400,
+// 				body: JSON.stringify({ message: '"lastName" must be a string' }),
+// 			};
+// 		}
+// 		if (!email || typeof email !== "string") {
+// 			return {
+// 				statusCode: 400,
+// 				body: JSON.stringify({ message: '"email" must be a string' }),
+// 			};
+// 		}
+// 		if (!password || typeof password !== "string") {
+// 			return {
+// 				statusCode: 400,
+// 				body: JSON.stringify({ message: '"password" must be a string' }),
+// 			};
+// 		}
+
+// 		// Check if the email is already registered in Cognito
+// 		try {
+// 			const userExists = await cognito
+// 				.adminGetUser({
+// 					UserPoolId: Resource.eccs.id,
+// 					Username: email,
+// 				})
+// 				.promise();
+
+// 			if (userExists) {
+// 				return {
+// 					statusCode: 400,
+// 					body: JSON.stringify({
+// 						message: "A user with this email already exists.",
+// 						error: "A user with this email already exists.",
+// 					}),
+// 				};
+// 			}
+// 		} catch (error) {
+// 			if (error.code !== "UserNotFoundException") {
+// 				console.error("Error checking for existing user:", error);
+// 				return {
+// 					statusCode: 500,
+// 					body: JSON.stringify({
+// 						message: "Error checking for existing user.",
+// 						error: `Error checking for existing user: ${error.message}`,
+// 					}),
+// 				};
+// 			}
+// 		}
+
+// 		// const duplicateCheckResponse = await checkDuplicateUsernameOrEmail(email);
+
+// 		// if (duplicateCheckResponse) {
+// 		// 	return duplicateCheckResponse;
+// 		// }
+
+// 		let teacherId;
+
+// 		// Fetch the single teacher's ID from DynamoDB
+// 		if (user_role === "student") {
+// 			const teacherParams = {
+// 				TableName: TABLES.USER,
+// 				IndexName: "RoleIndex", // Use user_role instead of role
+// 				KeyConditionExpression: "user_role = :user_role", // Use user_role
+// 				ExpressionAttributeValues: {
+// 					":user_role": "teacher",
+// 				},
+// 			};
+
+// 			const teacherCommand = new QueryCommand(teacherParams);
+// 			const teacherResult = await dbClient.send(teacherCommand);
+
+// 			if (!teacherResult.Items || teacherResult.Items.length === 0) {
+// 				return {
+// 					statusCode: 500, // TODO: check for correct status, it should be 400, not 500
+// 					body: JSON.stringify({
+// 						error: "No teacher found in the system.",
+// 					}),
+// 				};
+// 			}
+
+// 			// Get the first (and only) teacher's ID
+// 			teacherId = teacherResult.Items[0].id;
+// 		}
+
+// 		// Prepare the new user data
+// 		const originalPassword = decryptPassword(
+// 			password,
+// 			resources.NEXT_PUBLIC_PASS_SECRET_KEY
+// 		);
+// 		const hashedPassword = bcrypt.hashSync(originalPassword, 10);
+// 		const userId = uuidv4();
+// 		const createdAt = new Date().toISOString();
+
+// 		const user = {
+// 			id: userId,
+// 			firstName,
+// 			lastName,
+// 			email,
+// 			password: hashedPassword,
+// 			createdAt,
+// 			status: "active",
+// 			signUpLevel: 1,
+// 			paymentStatus: "inactive",
+// 			user_role,
+// 			teacherId: user_role === "student" ? teacherId : null,
+// 		};
+
+// 		const params = {
+// 			TableName: TABLES.USER,
+// 			Item: user,
+// 		};
+
+// 		const command = new PutCommand(params);
+// 		await dbClient.send(command);
+
+// 		// Remove password before sending response
+// 		delete user.password;
+
+// 		return {
+// 			statusCode: 201,
+// 			body: JSON.stringify({
+// 				message: "User was registered successfully!",
+// 				data: user,
+// 			}),
+// 		};
+// 	} catch (error) {
+// 		console.error("Error creating new user:", error);
+// 		return {
+// 			statusCode: 500,
+// 			body: JSON.stringify({
+// 				details: `Error creating new user: ${error.message}`,
+// 				message: "Error creating new user.",
+// 			}),
+// 		};
+// 	}
+// };
 
 export const signin = async (event) => {
 	try {
--- server/middlewares/verifySignUp.js
+++ server/middlewares/verifySignUp.js
@@ -2,43 +2,41 @@ import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 import dbClient from "../services/dbClient.js";
 import { TABLES } from "../services/dbTables.js";
 
-export const checkDuplicateUsernameOrEmail = async (event) => {
-	// TODO: expand check to make sure we are using pertinnent fields info to check for duplication
-	const { email } = JSON.parse(event.body);
+// THIS MAY NOT BE NECESSARY ANYMORE AS COGNITO HANDLES THIS!
+// export const checkDuplicateUsernameOrEmail = async (email) => {
+// 	try {
+// 		const params = {
+// 			TableName: TABLES.USER,
+// 			FilterExpression: "email = :email",
+// 			ExpressionAttributeValues: {
+// 				":email": email,
+// 			},
+// 		};
 
-	try {
-		const params = {
-			TableName: TABLES.USER,
-			FilterExpression: "email = :email",
-			ExpressionAttributeValues: {
-				":email": email,
-			},
-		};
+// 		const command = new ScanCommand(params);
+// 		const result = await dbClient.send(command);
 
-		const command = new ScanCommand(params);
-		const result = await dbClient.send(command);
+// 		const user = result.Items[0];
 
-		const user = result.Items[0];
+// 		if (user) {
+// 			return {
+// 				statusCode: 400,
+// 				body: JSON.stringify({
+// 					message: "Username or email already exists",
+// 					error: "Username or email already exists",
+// 				}),
+// 			};
+// 		}
 
-		if (user) {
-			return {
-				statusCode: 400,
-				body: JSON.stringify({
-					message: "Username or email already exists",
-					error: "Username or email already exists",
-				}),
-			};
-		}
-
-		return null;
-	} catch (error) {
-		console.error("Error checking for duplicate user", error);
-		return {
-			statusCode: 500,
-			body: JSON.stringify({
-				error: `Error checking for duplicate user: ${error.message}`,
-				message: `Error checking for duplicate user.`,
-			}),
-		};
-	}
-};
+// 		return null;
+// 	} catch (error) {
+// 		console.error("Error checking for duplicate user", error);
+// 		return {
+// 			statusCode: 500,
+// 			body: JSON.stringify({
+// 				error: `Error checking for duplicate user: ${error.message}`,
+// 				message: `Error checking for duplicate user.`,
+// 			}),
+// 		};
+// 	}
+// };
