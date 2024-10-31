9d647dbe4aed0516db07d3a6c689435555147d6b
Joshua Abu <otktechnologies@gmail.com>
2024-10-31 13:50:39 +0400

    add next auth callbacks for jwt and session

--- src/auth.ts
+++ src/auth.ts
@@ -1,59 +1,95 @@
 import NextAuth, { DefaultSession } from "next-auth";
+import NextAuthOptions from "next-auth";
+import { JWT } from "next-auth/jwt";
 import Cognito from "next-auth/providers/cognito";
 import { Resource } from "sst";
+import { verifyTokenFromCognito } from "../server/utils/verifyToken";
 
-declare module "next-auth" {
+declare module "next-auth/jwt" {
 	/**
-	 * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+	 * Extended JWT object to include access token and user properties
 	 */
-	interface Session {
-		user: {
-			/** The user's postal address. */
-			id: string;
-		} & DefaultSession["user"];
-		accessToken: string;
-	}
 	interface JWT {
-		/** OpenID ID Token */
 		accessToken?: string;
+		id?: string;
+		firstName?: string;
+		lastName?: string;
+		user_role?: string;
+		accessTokenExpires?: number;
+		refreshToken?: string;
 	}
 }
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-	// read here https://authjs.dev/getting-started/deployment#auth_trust_host
-	// trustHost: true,
+declare module "next-auth" {
+	/**
+	 * Extended user object to include custom properties
+	 */
+	interface User {
+		id?: string;
+		firstName?: string;
+		lastName?: string;
+		user_role?: string;
+	}
+
+	/**
+	 * Extended session object to include access token and user details
+	 */
+	interface Session extends DefaultSession {
+		accessToken?: string;
+		user: {
+			id: string;
+			firstName: string;
+			lastName: string;
+			user_role: string;
+		};
+	}
+}
+
+const authOptions = {
 	theme: { logo: "https://next-auth.js.org/img/logo/logo-sm.png" },
 	providers: [
 		Cognito({
 			clientId: Resource.eccswebclient.id,
 			clientSecret: Resource.eccswebclient.secret,
-			issuer:
-				"https://cognito-idp." +
-				process.env.NEXT_PUBLIC_REGION +
-				".amazonaws.com/" +
-				Resource.eccsuserpool.id,
+			issuer: `https://cognito-idp.${process.env.NEXT_PUBLIC_REGION}.amazonaws.com/${Resource.eccs.id}`,
 		}),
 	],
 	callbacks: {
-		// authorized({ request, auth }) {
-		// 	try {
-		// 		const { pathname } = request.nextUrl;
-		// 		console.log(pathname);
-		// 		if (pathname.startsWith("/protected-page")) return !!auth;
-		// 		return true;
-		// 	} catch (err) {
-		// 		console.log(err);
-		// 	}
-		// },
-		jwt({ token, account, profile, trigger, session }) {
-			if (trigger === "update") token.name = session.user.email;
+		async jwt({ token, account }) {
+			// Initial sign-in
+			if (account) {
+				token.accessToken = account.access_token;
+
+				// Verify and decode the ID token using verifyTokenFromCognito
+				const { isValid, decoded, error } = await verifyTokenFromCognito(
+					account.id_token!
+				);
+
+				if (isValid && decoded) {
+					// Populate token with user details
+					token.id = decoded.sub;
+					token.firstName = decoded.given_name;
+					token.lastName = decoded.family_name;
+					token.user_role = decoded["custom:user_role"];
+				} else {
+					console.error("Failed to verify ID token:", error);
+				}
+			}
+
 			return token;
 		},
-		session: async ({ session, token, user }) => {
+		async session({ session, token }) {
+			// Add accessToken and user details to the session object
 			session.accessToken = token.accessToken;
-
-			session.user.id = user.id;
+			session.user = {
+				id: token.id as string, // Use `as string` to ensure type compatibility
+				firstName: token.firstName as string,
+				lastName: token.lastName as string,
+				user_role: token.user_role as string,
+			};
 			return session;
 		},
 	},
-});
+};
+
+export default NextAuth(authOptions);
