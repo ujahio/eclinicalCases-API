91bf67305fa264fac7f1b59b4be81e93c22733ae
Joshua Abu <otktechnologies@gmail.com>
2024-11-01 19:30:14 +0400

    change name of sst app and client app

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
 	app(input) {
 		return {
-			name: "eccs-labs-app",
+			name: "eccs-labs",
 			removal: input?.stage === "production" ? "retain" : "remove",
 			home: "aws",
 			providers: {
