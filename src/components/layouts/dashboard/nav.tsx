4aa8f70014411f711b2bbae9ac9d1aa0d375d1a7
Joshua Abu <otktechnologies@gmail.com>
2024-11-06 20:11:39 +0400

    correct session usage to route to the correct settings page based on user role

--- src/components/layouts/dashboard/nav.tsx
+++ src/components/layouts/dashboard/nav.tsx
@@ -7,8 +7,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { APP_CONTAINER, APP_SPACING } from "@/services/constants/styles";
-import { useAppSelector } from "@/services/hooks/hooks";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 interface NavProps {
 	navLinks?: { path: string; label: string }[];
@@ -33,8 +32,8 @@ const Nav: FunctionComponent<NavProps> = ({
 }) => {
 	const router = useRouter();
 	const currentPath = usePathname();
-	const userInfo = useAppSelector((state) => state.login.user);
-
+	const { data } = useSession();
+	const { user_role } = data!.user;
 	const pathMatches = (path: string) => {
 		return currentPath === path;
 	};
@@ -72,14 +71,9 @@ const Nav: FunctionComponent<NavProps> = ({
 				</ul>
 
 				<div className="flex items-center cursor-pointer" role="button">
-					<figure className=" h-6.25 w-6.25 sm:h-8.75 sm:w-8.75 rounded-full">
+					<figure className=" h-6.25 w-6.25 sm:h-8.75 sm:w-8.75 rounded-full mr-3">
 						<Image src={UserImg} alt="" className="h-full w-full" />
 					</figure>
-					<span className="text-dark text-sm inline-block mx-2.5 font-medium">
-						{userInfo?.user?.user_role === "teacher"
-							? userInfo?.user?.email
-							: userInfo?.user?.email}
-					</span>
 					<svg
 						width="10.591"
 						height="6"
@@ -105,7 +99,7 @@ const Nav: FunctionComponent<NavProps> = ({
 							)}
 							<AppDropdownItem
 								href={
-									userInfo?.user?.user_role === "teacher"
+									user_role === "teacher"
 										? "/doctor/settings"
 										: "/student/settings"
 								}
