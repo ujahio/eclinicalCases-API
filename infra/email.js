452baeb954df6e0250fe0e62094eecf67d6fd0a9
Joshua Abu <otktechnologies@gmail.com>
2024-11-08 10:42:49 +0400

    conditionally setting domain and sender

--- infra/email.js
+++ infra/email.js
@@ -1,7 +1,9 @@
 const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
 
+const STAGE = $app.stage;
+
 export const ECCSEmail = new sst.aws.Email("ECCSEMAIL", {
-	domain: DOMAIN,
-	sender: DOMAIN,
-	mailFromDomain: `no-reply@${DOMAIN}`,
+	...(STAGE === "localdev" ? { domain: DOMAIN } : {}),
+	...(STAGE === "localdev" ? { sender: DOMAIN } : {}),
+	mailFromDomain: `${STAGE}-no-reply@${DOMAIN}`,
 });
