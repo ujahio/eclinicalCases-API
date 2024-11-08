a08c5579ad565bb7cb7d97703222e9ef48b3b295
Joshua Abu <otktechnologies@gmail.com>
2024-11-08 10:50:41 +0400

    get the email identity if its been deployed already

--- infra/email.js
+++ infra/email.js
@@ -2,8 +2,11 @@ const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
 
 const STAGE = $app.stage;
 
-export const ECCSEmail = new sst.aws.Email("ECCSEMAIL", {
-	...(STAGE === "localdev" ? { domain: DOMAIN } : {}),
-	...(STAGE === "localdev" ? { sender: DOMAIN } : {}),
-	mailFromDomain: `${STAGE}-no-reply@${DOMAIN}`,
-});
+export const ECCSEmail =
+	STAGE === "localdev"
+		? new sst.aws.Email("ECCSEMAIL", {
+				domain: DOMAIN,
+				sender: DOMAIN,
+				mailFromDomain: `no-reply@${DOMAIN}`,
+		  })
+		: sst.aws.Email.get(DOMAIN, DOMAIN);
