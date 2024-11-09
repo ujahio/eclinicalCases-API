5b0a48db14e3255932418c811288f09f811e8240
Joshua Abu <otktechnologies@gmail.com>
2024-11-09 23:25:28 +0400

    remove logo linking to home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,53 +7,53 @@ import Button from "./ui/Button";
 import AppDropdown, { AppDropdownItem } from "./ui/Dropdown";
 
 const Navbar = () => {
-  return (
-    <div>
-      <div className={`flex items-center justify-between bg-white shadow-sm navbar h-17.5 ${LANDING_X_PADDING}`}>
-        <div
-          className={`w-full flex items-center justify-between bg-white shadow-sm navbar h-17.5 ${LANDING_X_PADDING} ${APP_CONTAINER}`}
-        >
-          <Image src={Logo} alt="" className="w-auto h-5 sm:h-6" />
-          <ul className="text-dark md:flex hidden h-full items-center space-x-5 md:space-x-8">
-            <li className="h-full relative inline-flex items-center">
-              <Link href="/home" className="active uppercase">
-                Home
-              </Link>
-            </li>
-            {/* <li className="h-full relative inline-flex items-center">
+	return (
+		<div
+			className={`flex items-center justify-between bg-white shadow-sm navbar h-17.5 ${LANDING_X_PADDING}`}
+		>
+			<div
+				className={`w-full flex items-center justify-between bg-white shadow-sm navbar h-17.5 ${LANDING_X_PADDING} ${APP_CONTAINER}`}
+			>
+				<Image src={Logo} alt="" className="w-auto h-5 sm:h-6" />
+				<ul className="text-dark md:flex hidden h-full items-center space-x-5 md:space-x-8">
+					<li className="h-full relative inline-flex items-center">
+						<Link href="/home" className="active uppercase">
+							Home
+						</Link>
+					</li>
+					{/* <li className="h-full relative inline-flex items-center">
               <Link href="" className="uppercase">
                 Objectives
               </Link>
             </li> */}
-          </ul>
-          <div className="hidden md:flex">
-            <Button href="/login" className="mr-3 " btnStyle="outline" size="md">
-              Log in
-            </Button>
-            <Button href="/signup" size="md">
-              get started
-            </Button>
-          </div>
-          <div className="md:hidden">
-            <button className="outline-none focus:outline-none">
-              <div className=" w-6 sm:w-7 mb-1 h-0.5 bg-grey-300" />
-              <div className=" w-6 sm:w-7 mb-1 h-0.5 bg-grey-300" />
-              <div className=" w-6 sm:w-7 h-0.5 bg-grey-300" />
+				</ul>
+				<div className="hidden md:flex">
+					<Button href="/login" className="mr-3 " btnStyle="outline" size="md">
+						Log in
+					</Button>
+					<Button href="/signup" size="md">
+						get started
+					</Button>
+				</div>
+				<div className="md:hidden">
+					<button className="outline-none focus:outline-none">
+						<div className=" w-6 sm:w-7 mb-1 h-0.5 bg-grey-300" />
+						<div className=" w-6 sm:w-7 mb-1 h-0.5 bg-grey-300" />
+						<div className=" w-6 sm:w-7 h-0.5 bg-grey-300" />
 
-              <AppDropdown>
-                <AppDropdownItem href="/login" className="uppercase">
-                  Log in
-                </AppDropdownItem>
-                <AppDropdownItem href="/signup" className="uppercase">
-                  Get started
-                </AppDropdownItem>
-              </AppDropdown>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<AppDropdown>
+							<AppDropdownItem href="/login" className="uppercase">
+								Log in
+							</AppDropdownItem>
+							<AppDropdownItem href="/signup" className="uppercase">
+								Get started
+							</AppDropdownItem>
+						</AppDropdown>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
