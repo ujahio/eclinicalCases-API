b174d728dd97bbdae0c3001f8eac080d4b70bc52
Joshua Abu <otktechnologies@gmail.com>
2024-11-14 19:57:08 +0400

    more work on walkthrough modal

--- src/components/modals/Walkthrough.tsx
+++ src/components/modals/Walkthrough.tsx
@@ -80,26 +80,28 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 						Here are the steps to get started with e-Clinical Cases Solutions.
 					</p>
 				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image
-							src={register}
-							alt=""
-							className="h-full mx-auto"
-							width={1100}
-						/>
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 1
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							Click on the &quot;Get Started&quot; button to create an account.
-							You will recieve a verification email to activate your account.
-						</p>
+				{/* template li */}
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 1.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Enter the basic information to start registration. You will recieve
+						a verification email to activate your account.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={register}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
 					</div>
 				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
+				{/* rest of the li elements */}
+				{/* <li className="md:flex py-16 sm:py-10 justify-self-center">
 					<figure className="md:h-auto sm:h-64 h-48">
 						<Image
 							src={signin}
@@ -218,6 +220,163 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
 							Give feedback on the course and download the certificate
 						</p>
+					</div>
+				</li> */}
+
+				{/* Refactored li elements */}
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 2.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Login in with your verified email address.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={signin}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10 mr-5 ml-5">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 3.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						The most recent ongoing case study will be displayed on your
+						dashboard. Click on the "View Case" button to view the details.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={dashboard}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 4.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Review the case presentation for the ongoing case study. After
+						careful evaluation, click on "Proceed to the Case Model Question" to
+						respond to the case presentation.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={casepresentation}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 5.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Give a response to the case study based on the case presentation.
+						The max count is 700 words.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={caseresponse}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 6.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Compare your answers to the teacher's case model answer.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={casemodelanswer}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 7.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						In order to get a certificate, you must score a 100% on the CME
+						Questions. You can retake the test as many times as you want.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={screen}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 8.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Give feedback on the course and download the certificate.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={screen}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 9.
+					</h2>
+					<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Give feedback on the course and download the certificate.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={screen}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
 					</div>
 				</li>
 			</ul>
