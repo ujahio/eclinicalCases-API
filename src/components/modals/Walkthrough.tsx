da4013ec709511a340cc0409aa3662c055accd7b
Joshua Abu <otktechnologies@gmail.com>
2024-11-15 18:22:55 +0400

    finished up added the pngs for each step in walkthrough modal

--- src/components/modals/Walkthrough.tsx
+++ src/components/modals/Walkthrough.tsx
@@ -5,22 +5,24 @@ import Modal, { ModalProps } from "../ui/Modal";
 import Button from "../ui/Button";
 
 import LogoImg from "@/assets/images/logo.png";
-import screen from "@/assets/images/screen-1.svg";
+import certificate from "@/assets/images/certicate.png";
 import register from "@/assets/images/register.png";
 import signin from "@/assets/images/signin.png";
 import dashboard from "@/assets/images/dashboard.png";
 import casepresentation from "@/assets/images/casepresentation.png";
 import caseresponse from "@/assets/images/caseresponse.png";
 import casemodelanswer from "@/assets/images/casemodelanswer.png";
-
+import caseteaching from "@/assets/images/caseteaching.png";
+import cmequestions from "@/assets/images/cmequestionspage.png";
+import feedback from "@/assets/images/feedback.png";
 const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 	const [progress, setProgress] = useState(0);
 	const sliderList = useRef<HTMLUListElement>(null);
-	let steps = 10;
+	let steps = 11;
 
 	const changeSlide = (direction: "previous" | "next") => {
 		let nextSlide;
-		const changeSlideStep = 10;
+		const changeSlideStep = 11;
 
 		if (direction === "next") {
 			nextSlide = progress >= changeSlideStep - 1 ? 0 : progress + 1;
@@ -64,7 +66,7 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 			<ul
 				className="grid overflow-hidden"
 				style={{
-					gridTemplateColumns: "repeat(10, 100%)",
+					gridTemplateColumns: "repeat(11, 100%)",
 					scrollBehavior: "smooth",
 				}}
 				ref={sliderList}
@@ -80,7 +82,6 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 						Here are the steps to get started with e-Clinical Cases Solutions.
 					</p>
 				</li>
-				{/* template li */}
 				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
 					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
 						Step 1.
@@ -100,130 +101,7 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 						</figure>
 					</div>
 				</li>
-				{/* rest of the li elements */}
-				{/* <li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image
-							src={signin}
-							alt=""
-							className="h-full mx-auto"
-							width={1100}
-						/>
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 2
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							Login in with your verified email address.
-						</p>
-					</div>
-				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image
-							src={dashboard}
-							alt=""
-							className="h-full mx-auto"
-							width={1500}
-							// height={1000}
-						/>
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 3
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							The most recent ongoing case study will be displayed on your
-							dashboard. Click on the &quot;View Case&quot; button to view the
-							details.
-						</p>
-					</div>
-				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image src={casepresentation} alt="" className="h-full mx-auto" />
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 4
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							Review the case presentation for the ongoing case study. After
-							careful evaluation, click on &quot;Proceed to the Case Model
-							Question&quot; to respond to the case presentation.
-						</p>
-					</div>
-				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image src={caseresponse} alt="" className="h-full mx-auto" />
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 5
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							Give a response to the case study based on the case presentation.
-							The max count is 700 words.
-						</p>
-					</div>
-				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image src={casemodelanswer} alt="" className="h-full mx-auto" />
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 6
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							Compare your answers to the teacher&apos;s case model answer.
-						</p>
-					</div>
-				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image src={screen} alt="" className="h-full mx-auto" />
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 7
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							In order to get a certificate, you must score a 100% on the CME
-							Questions. You can retake the test as many times as you want.
-						</p>
-					</div>
-				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image src={screen} alt="" className="h-full mx-auto" />
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 8
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							Give feedback on the course and download the certificate
-						</p>
-					</div>
-				</li>
-				<li className="md:flex py-16 sm:py-10 justify-self-center">
-					<figure className="md:h-auto sm:h-64 h-48">
-						<Image src={screen} alt="" className="h-full mx-auto" />
-					</figure>
-					<div className="ml-3">
-						<h2 className="text-md sm:text-lg font-medium mt-4 md:mt-14 text-dark">
-							Step 9
-						</h2>
-						<p className="text-xs sm:text-sm text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-							Give feedback on the course and download the certificate
-						</p>
-					</div>
-				</li> */}
 
-				{/* Refactored li elements */}
 				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
 					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
 						Step 2.
@@ -308,7 +186,7 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 						Step 6.
 					</h2>
 					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Compare your answers to the teacher's case model answer.
+						Compare your response to the teacher's case model answer.
 					</p>
 					<div className="mt-4">
 						<figure className="md:h-auto sm:h-64 h-48">
@@ -327,13 +205,14 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 						Step 7.
 					</h2>
 					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						In order to get a certificate, you must score a 100% on the CME
-						Questions. You can retake the test as many times as you want.
+						Read through the teachers case teaching on the subject in details.
+						The teaching also contains additional resources for further
+						learning.
 					</p>
 					<div className="mt-4">
 						<figure className="md:h-auto sm:h-64 h-48">
 							<Image
-								src={screen}
+								src={caseteaching}
 								alt=""
 								className="h-full mx-auto"
 								width={700}
@@ -347,12 +226,13 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 						Step 8.
 					</h2>
 					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Give feedback on the course and download the certificate.
+						Answer all the multiple choice questions correctly to complete the
+						course and earn your certificate.{" "}
 					</p>
 					<div className="mt-4">
 						<figure className="md:h-auto sm:h-64 h-48">
 							<Image
-								src={screen}
+								src={cmequestions}
 								alt=""
 								className="h-full mx-auto"
 								width={700}
@@ -371,7 +251,26 @@ const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 					<div className="mt-4">
 						<figure className="md:h-auto sm:h-64 h-48">
 							<Image
-								src={screen}
+								src={feedback}
+								alt=""
+								className="h-full mx-auto"
+								width={700}
+							/>
+						</figure>
+					</div>
+				</li>
+
+				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
+					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
+						Step 10.
+					</h2>
+					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
+						Download your certificate.
+					</p>
+					<div className="mt-4">
+						<figure className="md:h-auto sm:h-64 h-48">
+							<Image
+								src={certificate}
 								alt=""
 								className="h-full mx-auto"
 								width={700}
