75081e722a3fbfbd1a6dcb5171cf770166d2f2b3
Joshua Abu <otktechnologies@gmail.com>
2024-11-27 17:10:04 +0400

    changed some imports; abstract emailSender

---
+++ server/email/emailSender.ts
@@ -0,0 +1,46 @@
+import { SendEmailCommand } from "@aws-sdk/client-sesv2";
+import applicationContext from "../../applicationContext";
+
+type EmailHtmlProps = {
+	Html: {
+		Data: string;
+	};
+};
+
+export type EmailProps = {
+	sender: string;
+	recipients: string[];
+	subject: string;
+	body: EmailHtmlProps;
+};
+
+export const sendEmail = async ({
+	recipients,
+	subject,
+	body,
+	sender,
+}: EmailProps) => {
+	const sesClient = applicationContext.getMessagingClient();
+
+	try {
+		await sesClient.send(
+			new SendEmailCommand({
+				FromEmailAddress: sender,
+				Destination: {
+					ToAddresses: recipients,
+				},
+				Content: {
+					Simple: {
+						Subject: {
+							Data: subject,
+						},
+						Body: body,
+					},
+				},
+			})
+		);
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+};
