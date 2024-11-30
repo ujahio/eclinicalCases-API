0524293557f7bb786e89a1c9c9f543d4c1371c29
Joshua Abu <otktechnologies@gmail.com>
2024-11-30 20:56:13 +0400

    remove redux-persist related packages and implementation as not needed anymore. Clean up redux middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,49 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer, { RootState } from "./rootReducer/rootReducer";
-import createFilter from "redux-persist-transform-filter";
-import {
-	persistReducer,
-	persistStore,
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-const loginFilter = createFilter("login", ["isLoggedIn", "user"]);
-const persistConfig = {
-	key: "eclinical",
-	storage,
-	whitelist: ["login"],
-	transforms: [loginFilter],
-};
-
-const persistedReducer = persistReducer<RootState>(persistConfig, rootReducer);
+import rootReducer from "./rootReducer/rootReducer";
 
 const isDevelopment = process.env.NEXT_PUBLIC_NODE_ENV === "development";
 
 export const makeStore = () => {
-	let store: any = configureStore({
-		reducer: persistedReducer,
+	let store = configureStore({
+		reducer: rootReducer,
 		devTools:
 			isDevelopment && typeof window !== "undefined"
 				? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
 				  (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 				: undefined,
-		middleware: (getDefaultMiddleware) =>
-			getDefaultMiddleware({
-				serializableCheck: {
-					ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-				},
-			}),
 	});
-	store.__persistor = persistStore(store);
 	return store;
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
-// export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
