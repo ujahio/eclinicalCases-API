86b204847d5f4364ceb0198030189e8a02da2f2c
Joshua Abu <otktechnologies@gmail.com>
2024-11-30 21:10:30 +0400

    clean up instatiation of devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer/rootReducer";
 
-const isDevelopment = process.env.NEXT_PUBLIC_NODE_ENV === "development";
+const isDevelopment =
+	process.env.NEXT_PUBLIC_NODE_ENV !== "production" &&
+	process.env.NEXT_PUBLIC_NODE_ENV !== "preproduction";
 
 export const makeStore = () => {
 	let store = configureStore({
 		reducer: rootReducer,
-		devTools:
-			isDevelopment && typeof window !== "undefined"
-				? (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-				  (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-				: undefined,
+		devTools: isDevelopment,
 	});
 	return store;
 };
