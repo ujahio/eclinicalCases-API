9c55abc433e629ea26eef7a363de2c12959fcd16
Joshua Abu <otktechnologies@gmail.com>
2024-12-09 01:45:47 +0400

    temp disable starting the webserver to autostart before tests are ran; add default url setting and viewport setting for chrome

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		// baseURL: 'http://127.0.0.1:3000',
+		baseURL: "http://127.0.0.1:3000",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
@@ -34,20 +34,20 @@ export default defineConfig({
 
 	/* Configure projects for major browsers */
 	projects: [
-		{
-			name: "chromium",
-			use: { ...devices["Desktop Chrome"] },
-		},
+		// {
+		// 	name: "chromium",
+		// 	use: { ...devices["Desktop Chrome"] },
+		// },
 
-		{
-			name: "firefox",
-			use: { ...devices["Desktop Firefox"] },
-		},
+		// {
+		// 	name: "firefox",
+		// 	use: { ...devices["Desktop Firefox"] },
+		// },
 
-		{
-			name: "webkit",
-			use: { ...devices["Desktop Safari"] },
-		},
+		// {
+		// 	name: "webkit",
+		// 	use: { ...devices["Desktop Safari"] },
+		// },
 
 		/* Test against mobile viewports. */
 		// {
@@ -66,14 +66,18 @@ export default defineConfig({
 		// },
 		{
 			name: "Google Chrome",
-			use: { ...devices["Desktop Chrome"], channel: "chrome" },
+			use: {
+				...devices["Desktop Chrome"],
+				channel: "chrome",
+				viewport: { width: 1600, height: 988 },
+			},
 		},
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: "npx sst dev --stage localdev",
-		url: "http://127.0.0.1:3000",
-		reuseExistingServer: !process.env.CI,
-	},
+	// webServer: {
+	// 	command: "npm run start:sst",
+	// 	url: "http://127.0.0.1:3000",
+	// 	reuseExistingServer: !process.env.CI,
+	// },
 });
