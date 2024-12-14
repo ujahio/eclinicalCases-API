d33bbe51972cdaccf1ba6a9e8e99acc1f39fec71
Joshua Abu <otktechnologies@gmail.com>
2024-12-15 00:42:25 +0400

    correct base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,14 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 const STAGE = Resource.NEXT_PUBLIC_STAGE.value;
 console.log("STAGE", STAGE);
+
+const BASE_URL =
+	STAGE === "preproduction" || STAGE === "production"
+		? `https://${STAGE}.${process.env.NEXT_PUBLIC_DOMAIN}`
+		: "http://127.0.0.1:3000";
+
+console.log("BASE_URL", BASE_URL);
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -31,11 +39,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL:
-			STAGE === "preproduction" || STAGE === "production"
-				? `https://${STAGE}${process.env.NEXT_PUBLIC_DOMAIN}`
-				: "http://127.0.0.1:3000",
-
+		baseURL: BASE_URL,
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
 	},
