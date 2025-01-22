e2e436b6352d07492e67fc8a3024159c56a930b0
Joshua Abu <otktechnologies@gmail.com>
2025-01-22 13:34:57 +0400

    changed nav item from certification to certificates

--- src/components/layouts/dashboard/nav.tsx
+++ src/components/layouts/dashboard/nav.tsx
@@ -24,7 +24,7 @@ const Nav: FunctionComponent<NavProps> = ({
 			path: "/student/dashboard",
 		},
 		{
-			label: "Certifications",
+			label: "Certificates",
 			path: "/student/certificates",
 		},
 	],
