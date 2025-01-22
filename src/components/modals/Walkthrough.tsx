47a61ef6487214e782ec1d1110b45dc689822daa
Joshua Abu <otktechnologies@gmail.com>
2025-01-22 13:29:16 +0400

    adjust Modals to properly and dynamically render items

--- src/components/modals/Walkthrough.tsx
+++ src/components/modals/Walkthrough.tsx
@@ -1,303 +1,211 @@
-import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+import React, { FunctionComponent, useState } from "react";
 import Image from "next/image";
 
 import Modal, { ModalProps } from "../ui/Modal";
 import Button from "../ui/Button";
 
 import LogoImg from "@/assets/images/logo.png";
-import certificate from "@/assets/images/certicate.png";
-import register from "@/assets/images/register.png";
-import signin from "@/assets/images/signin.png";
-import dashboard from "@/assets/images/dashboard.png";
-import casepresentation from "@/assets/images/casepresentation.png";
-import casecomments from "@/assets/images/casecomments.png";
-import casemodelanswer from "@/assets/images/casemodelanswer.png";
-import caseteaching from "@/assets/images/caseteaching.png";
-import cmequestions from "@/assets/images/cmequestionspage.png";
-import feedback from "@/assets/images/feedback.png";
+import CertificateImg from "@/assets/images/certicate.png";
+import RegisterImg from "@/assets/images/register.png";
+import SigninImg from "@/assets/images/signin.png";
+import DashboardImg from "@/assets/images/dashboard.png";
+import CasepresentationImg from "@/assets/images/casepresentation.png";
+import CasecommentsImg from "@/assets/images/casecomments.png";
+import CaseModalAnswerImg from "@/assets/images/casemodelanswer.png";
+import CaseTeachingImg from "@/assets/images/caseteaching.png";
+import CmeQuestionsImg from "@/assets/images/cmequestionspage.png";
+import FeedbackImg from "@/assets/images/feedback.png";
+
+interface Step {
+	step: number;
+	title: string;
+	description: string;
+	image: string;
+	alt: string;
+}
+
+const stepsData: Step[] = [
+	{
+		step: 0,
+		title: "Welcome",
+		description:
+			"Welcome to e-Clinical Cases Solutions. Here are the steps to get started.",
+		image: LogoImg.src,
+		alt: "e Clinical Cases Solutions logo",
+	},
+	{
+		step: 1,
+		title: "Step 1.",
+		description:
+			"Enter the basic information to start the registration of your new account. You will receive a verification email to complete the registration.",
+		image: RegisterImg.src,
+		alt: "registration image",
+	},
+	{
+		step: 2,
+		title: "Step 2.",
+		description: "Login in with your verified email address.",
+		image: SigninImg.src,
+		alt: "sign in image",
+	},
+	{
+		step: 3,
+		title: "Step 3.",
+		description:
+			'Click on the "View Case" button to review the details of the recently published case.',
+		image: DashboardImg.src,
+		alt: "student dashboard image",
+	},
+	{
+		step: 4,
+		title: "Step 4.",
+		description:
+			"Review the presentation of the current case study. After careful evaluation, proceed to comment on the presentation.",
+		image: CasepresentationImg.src,
+		alt: "case presentation image",
+	},
+	{
+		step: 5,
+		title: "Step 5.",
+		description:
+			"Comment on the case study based on the case presentation. Comments must be between 150 and 700 characters.",
+		image: CasecommentsImg.src,
+		alt: "case comments image",
+	},
+	{
+		step: 6,
+		title: "Step 6.",
+		description: "Compare your response to the teacher's case model answer.",
+		image: CaseModalAnswerImg.src,
+		alt: "case model answer image",
+	},
+	{
+		step: 7,
+		title: "Step 7.",
+		description:
+			"Read through the teacher's detailed case teaching on the subject. The teaching also contains additional resources for further learning.",
+		image: CaseTeachingImg.src,
+		alt: "case teaching image",
+	},
+	{
+		step: 8,
+		title: "Step 8.",
+		description:
+			"Answer all the multiple choice questions correctly to complete the course and earn your certificate.",
+		image: CmeQuestionsImg.src,
+		alt: "cme questions image",
+	},
+	{
+		step: 9,
+		title: "Step 9.",
+		description: "Give your feedback on the case study.",
+		image: FeedbackImg.src,
+		alt: "feedback image",
+	},
+	{
+		step: 10,
+		title: "Step 10.",
+		description: "Download your certificate.",
+		image: CertificateImg.src,
+		alt: "download certificate image",
+	},
+];
 
 const WalkthroughModal: FunctionComponent<ModalProps> = ({ show, toggle }) => {
 	const [progress, setProgress] = useState(0);
-	const sliderList = useRef<HTMLUListElement>(null);
-	let steps = 11;
+	const totalSteps = stepsData.length - 1;
 
 	const changeSlide = (direction: "previous" | "next") => {
 		let nextSlide;
-		const changeSlideStep = 11;
-
 		if (direction === "next") {
-			nextSlide = progress >= changeSlideStep - 1 ? 0 : progress + 1;
+			nextSlide = progress >= totalSteps ? 0 : progress + 1;
 		} else {
-			nextSlide = progress === 0 ? changeSlideStep - 1 : progress - 1;
+			nextSlide = progress === 0 ? totalSteps : progress - 1;
 		}
 		setProgress(nextSlide);
 	};
 
-	const getArrayOfSteps = () => {
-		const results = [];
-		for (let i = 0; i < steps; steps = -1) results.push(i);
-		return results;
-	};
-
-	const populateSteps = () => {
-		const arrayOfSteps = getArrayOfSteps();
-		return arrayOfSteps.map((step) => (
-			<li key={step}>
-				<button
-					aria-label={`${step}`}
-					onClick={() => setProgress(step)}
-					className={`no-outline h-1.5 w-1.5 rounded-full ${
-						step === progress ? "" : "bg-grey-400"
-					}`}
-				/>
-			</li>
-		));
-	};
-
-	useEffect(() => {
-		const listEl = sliderList.current;
-		if (listEl) {
-			const scrollPosition = listEl.children[0].clientWidth * progress;
-			listEl.scrollTo(scrollPosition, 0);
-		}
-	}, [progress]);
+	const currentStep = stepsData.find((s) => s.step === progress);
 
 	return (
-		<Modal {...{ show, toggle, size: "lg" }}>
-			<ul
-				className="grid overflow-hidden"
-				style={{
-					gridTemplateColumns: "repeat(11, 100%)",
-					scrollBehavior: "smooth",
-				}}
-				ref={sliderList}
-			>
-				<li className="flex flex-col items-center py-16 sm:py-24 md:py-30 mt-20">
-					<figure className="h-6">
-						<Image
-							src={LogoImg}
-							alt="e clinical cases solutions logo"
-							className="h-full w-auto"
-						/>
-					</figure>
-					<h2 className="text-lg font-medium mt-5 text-dark text-center">
-						Welcome to e-Clinical Cases Solutions
-					</h2>
-					<p className="text-sm text-dark mt-6 max-w-lg text-center">
-						Here are the steps to get started with e-Clinical Cases Solutions.
-					</p>
-				</li>
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 1.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Enter the basic information to start the registration of your new
-						account. You will receive a verification email to complete the
-						registration.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={register}
-								alt="registration image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
+		<Modal show={show} toggle={toggle} size="lg">
+			{/* Current Step Content */}
+			{currentStep && (
+				<div className="flex flex-col items-center py-16">
+					{currentStep.step === 0 && (
+						<>
+							<figure className="h-6 mb-5">
+								<Image
+									src={currentStep.image}
+									alt={currentStep.alt}
+									className="h-full w-auto"
+									width={150}
+									height={40}
+								/>
+							</figure>
+							<h2 className="text-lg font-medium mt-5 text-dark text-center">
+								{currentStep.description}
+							</h2>
+						</>
+					)}
+					{currentStep.step > 0 && (
+						<>
+							<h2 className="text-md sm:text-lg font-medium mt-3 text-dark text-center">
+								{currentStep.title}
+							</h2>
+							<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6 text-center">
+								{currentStep.description}
+							</p>
+							<div className="mt-4">
+								<figure className="md:h-auto sm:h-64 h-48">
+									<Image
+										src={currentStep.image}
+										alt={currentStep.alt}
+										className="h-full mx-auto"
+										width={700}
+										height={400}
+									/>
+								</figure>
+							</div>
+						</>
+					)}
+				</div>
+			)}
 
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 2.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Login in with your verified email address.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={signin}
-								alt="sign in image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10 mr-5 ml-5">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 3.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Click on the &quot;View Case&quot; button to review the details of
-						the recently published case.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={dashboard}
-								alt="student dashboard image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 4.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Review the presentation of the current case study. After careful
-						evaluation, proceed to comment on the presentation.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={casepresentation}
-								alt="case presentation image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 5.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Comment on the case study based on the case presentation. Comments
-						must be between 150 and 700 characters.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={casecomments}
-								alt="case comments image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 6.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Compare your response to the teacher&apos;s case model answer.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={casemodelanswer}
-								alt="case model answer image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 7.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Read through the teacher&apos;s detailed case teaching on the
-						subject. The teaching also contains additional resources for further
-						learning.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={caseteaching}
-								alt="case teaching image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 8.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Answer all the multiple choice questions correctly to complete the
-						course and earn your certificate.{" "}
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={cmequestions}
-								alt="cme questions image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 9.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Give your feedback on the case study.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={feedback}
-								alt="feedback image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-
-				<li className="md:flex flex-col justify-self-center py-6 mb-4 mt-10">
-					<h2 className="text-md sm:text-lg font-medium mt-3 text-dark">
-						Step 10.
-					</h2>
-					<p className="text-md sm:text-lg text-dark mt-3 max-w-lg tracking-wider sm:leading-7 leading-6">
-						Download your certificate.
-					</p>
-					<div className="mt-4">
-						<figure className="md:h-auto sm:h-64 h-48">
-							<Image
-								src={certificate}
-								alt="download certificate image"
-								className="h-full mx-auto"
-								width={700}
-							/>
-						</figure>
-					</div>
-				</li>
-			</ul>
-			<div className="flex items-center justify-between">
+			{/* Navigation Buttons */}
+			<div className="flex items-center justify-between mt-4">
 				{progress > 0 && (
 					<Button
 						btnStyle="outline"
-						size="sm"
+						size="md"
 						onClick={() => changeSlide("previous")}
 					>
 						Previous
 					</Button>
 				)}
-				<ul className="flex items-center space-x-2.5">{populateSteps()}</ul>
-				<Button btnStyle="basic" size="sm" onClick={() => changeSlide("next")}>
-					Next
-				</Button>
+				{/* Step Indicators */}
+				<ul className="flex items-center space-x-2.5">
+					{stepsData.map((stepData) => (
+						<li key={stepData.step}>
+							<button
+								aria-label={`Step ${stepData.step}`}
+								onClick={() => setProgress(stepData.step)}
+								className={`no-outline h-3 w-3 rounded-full ${
+									stepData.step === progress ? "bg-primary-500" : "bg-grey-400"
+								}`}
+							/>
+						</li>
+					))}
+				</ul>
+				{progress < totalSteps && (
+					<Button
+						btnStyle="basic"
+						size="md"
+						onClick={() => changeSlide("next")}
+					>
+						Next
+					</Button>
+				)}
 			</div>
 		</Modal>
 	);
