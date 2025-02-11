9dfacb69c1acbba53aa27ccf1e3f2ec595bfa3ab
Joshua Abu <otktechnologies@gmail.com>
2025-02-12 01:42:27 +0400

    sign out user if access token is invalid hook

--- src/services/hooks/useAuthRedirect.ts
+++ src/services/hooks/useAuthRedirect.ts
@@ -1,25 +1,19 @@
 import { useEffect } from "react";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-// Custom hook to handle user authentication and redirect if unauthenticated
 export const useAuthRedirect = () => {
 	const { data: session, status } = useSession();
 	const router = useRouter();
-
 	useEffect(() => {
-		if (
-			status === "unauthenticated" ||
-			!session?.user ||
-			!session?.accessToken
-		) {
-			// Redirect to login page or sign out if user data is missing
-			router.push("/login");
-			// Alternatively, you can sign out if needed
-			// signOut();
+		if (status === "loading") {
+			// Do nothing while loading
+			return;
 		}
-	}, [status, session, router]);
-
-	// Return session data and loading state
+		if (!session?.accessToken || session?.error === "RefreshAccessTokenError") {
+			// If there is no access token, sign out and redirect to login
+			signOut({ callbackUrl: "/login" });
+		}
+	}, [session, status, router]);
 	return { session, status };
 };
