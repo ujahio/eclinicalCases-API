0e46652c8b4d62843819e474ad6d7c7206ba63ad
Joshua Abu <joshujah007@gmail.com>
2025-02-21 22:56:24 +0400

    111 - update sst

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -2,8 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 /* deno-fmt-ignore-file */
-import "sst"
-export {}
+
 declare module "sst" {
   export interface Resource {
     "AUTH_SECRET": {
@@ -70,3 +69,7 @@ declare module "sst" {
     }
   }
 }
+/// <reference path="sst-env.d.ts" />
+
+import "sst"
+export {}
