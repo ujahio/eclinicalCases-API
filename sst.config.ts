1dd19c27c6318913e1c5c653c088c6a1ada08668
Joshua Abu <joshujah007@gmail.com>
2025-03-12 18:49:35 +0400

    dep-updates: patch updates including sst; upgrade pulumi aws package

--- sst.config.ts
+++ sst.config.ts
@@ -1,31 +1,31 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-	app(input) {
-		return {
-			name: "eccs-labs",
-			removal: input?.stage === "production" ? "retain" : "remove",
-			home: "aws",
-			providers: {
-				aws: {
-					region: "me-south-1",
-					version: "6.69.0",
-				},
-			},
-		};
-	},
-	async run() {
-		// Bucket
-		await import("./infra/client");
-		// Bucket
-		await import("./infra/storage");
-		// API
-		await import("./infra/api");
-		// Tables
-		await import("./infra/dynamo");
-		// Auth
-		await import("./infra/auth");
-		// Archived Cases Cron
-		await import("./infra/archive-case-status");
-	},
+  app(input) {
+    return {
+      name: "eccs-labs",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+      providers: {
+        aws: {
+          region: "me-south-1",
+          version: "6.71.0",
+        },
+      },
+    };
+  },
+  async run() {
+    // Bucket
+    await import("./infra/client");
+    // Bucket
+    await import("./infra/storage");
+    // API
+    await import("./infra/api");
+    // Tables
+    await import("./infra/dynamo");
+    // Auth
+    await import("./infra/auth");
+    // Archived Cases Cron
+    await import("./infra/archive-case-status");
+  },
 });
