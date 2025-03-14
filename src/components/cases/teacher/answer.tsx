8475d0c95f733a070b1e5ed7ad711f7377c3a86d
Joshua Abu <joshujah007@gmail.com>
2025-03-14 18:42:57 +0400

    109 - moved format Date to utils file

--- src/components/cases/teacher/answer.tsx
+++ src/components/cases/teacher/answer.tsx
@@ -4,14 +4,7 @@ import Button from "@/components/ui/Button";
 import { useAppSelector } from "@/services/hooks/hooks";
 import { TeacherCaseAnswerProps } from "@/services/types/teacher/createCaseStudy";
 import CaseEditor from "@/lib/Editor";
-
-const formatDateToYYYYMMDD = (dateString: any) => {
-	const date = new Date(dateString);
-	const year = date.getFullYear();
-	const month = String(date.getMonth() + 1).padStart(2, "0");
-	const day = String(date.getDate()).padStart(2, "0");
-	return `${year}-${month}-${day}`;
-};
+import { formatDateToYYYYMMDD } from "@/utils/formatDate";
 
 const TeacherCaseAnswer: FunctionComponent<TeacherCaseAnswerProps> = ({
 	goNext,
--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,16 @@
 export const formatDate = (timestamp: number | string) => {
-  const date = new Date(timestamp);
-  return date.toLocaleDateString();
+	const date = new Date(timestamp);
+	return date.toLocaleDateString();
+};
+
+export const formatDateToYYYYMMDD = (dateString: string) => {
+	const date = new Date(dateString);
+
+	if (date.toISOString() === dateString) {
+		const year = date.getFullYear();
+		const month = String(date.getMonth() + 1).padStart(2, "0");
+		const day = String(date.getDate()).padStart(2, "0");
+		return `${year}-${month}-${day}`;
+	}
+	return dateString;
 };
