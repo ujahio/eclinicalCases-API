3c21eb53ef81535c30ebd4d5f8549e330e39c5b3
otktechnologies <otktechnologies@gmail.com>
2026-06-23 16:20:33 -0500

    change api service name

--- src/services/apis/payment.ts
+++ src/services/apis/payment.ts
@@ -1,9 +1,13 @@
 import { studentApi, configureRequestHeaders } from "../config/fetchClient";
 
 export const createPaymentCheckout = async (paymentType: string) => {
-  return studentApi.post("/payment/checkout", { paymentType }, configureRequestHeaders());
+	return studentApi.post(
+		"/payment/checkout",
+		{ paymentType },
+		configureRequestHeaders(),
+	);
 };
 
-export const checkSubscriptionStatus = async () => {
-  return studentApi.get("/payment/status", configureRequestHeaders());
+export const checkPaymentStatus = async () => {
+	return studentApi.get("/payment/status", configureRequestHeaders());
 };
